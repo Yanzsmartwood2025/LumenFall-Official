611d23c78451766be721266d5ff71b417031455c
Jusn38 <yanzsmartwood@gmail.com>
2025-12-29 16:17:34 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
+// Aquí dentro irá tu juego o tu menú luego.
+// Por ahora ponemos esto para que el error 404 desaparezca.
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <div style={{ backgroundColor: 'black', color: 'cyan', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+    <h1>SISTEMA ONLINE</h1>
+  </div>
+)
